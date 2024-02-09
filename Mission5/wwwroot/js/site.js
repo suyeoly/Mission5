33452b14f8e009d8ec40d182bd4057f19105a3f1
suyeol <suyeol@SUYEOL5544>
2024-02-08 19:34:00 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var rate = parseFloat($('#rate').val());
+
+        if (hours > 0) {
+            var total = hours * rate;
+            $('#total').val(total.toFixed(2));
+        } else {
+            alert("Please enter a positive number of hours.");
+        }
+    });
+});
